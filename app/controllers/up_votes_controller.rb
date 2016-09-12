ac1a62b38e04d4c2f711e72a6705d6ae1fb8b7d6
Jahmal Lewis <Jahmal23@gmail.com>
2016-09-12 13:07:22 -0500

    up vote fixes

--- app/controllers/up_votes_controller.rb
+++ app/controllers/up_votes_controller.rb
@@ -2,24 +2,22 @@ class UpVotesController < ApplicationController
 
 
   def create
-
-    @vote = UpVote.new(vote_params)
+    @vote = current_user.up_votes.new(vote_params)
 
     @vote.user = current_user
 
+    post = Post.find(post_params[:post_id])
+
+    post.up_votes << @vote
 
     if @vote.save
       flash[:success] = 'Vote created'
-
     else
       flash[:error] = 'Vote could not be created'
-
     end
 
     redirect_to root_path
   end
-
-
 
   private
 
@@ -27,6 +25,8 @@ class UpVotesController < ApplicationController
     params.require(:up_vote).permit(:votable_type, :votable_id)
   end
 
-
+  def post_params
+    params.permit(:post_id)
+  end
 
 end
